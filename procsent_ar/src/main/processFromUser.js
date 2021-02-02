26388c15c99d315d5769e550647247dcb8cfd89a
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-02-02 19:02:54 +0200

    R

--- procsent_ar/src/main/processFromUser.js
+++ procsent_ar/src/main/processFromUser.js
@@ -14,8 +14,8 @@ export default class ProcessFromUser extends Component {
                         </div>
                     </div>
                     <div className="textTUsers">
+                   <i className='fas fa-fire' style={{ color: '#eb561b', fontSize: '15px' }}></i> &nbsp;
                         {this.props.messToApper}
-                         &nbsp;&nbsp;<i className='fas fa-fire' style={{ color: '#eb561b', fontSize: '15px' }}></i>
                     </div>
                 </div>
             </div>

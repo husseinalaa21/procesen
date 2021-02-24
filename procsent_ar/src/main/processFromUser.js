6f3959cd1cb149b192b90999a489ebf3c8b12f79
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-02-24 17:35:32 +0200

    F

--- procsent_ar/src/main/processFromUser.js
+++ procsent_ar/src/main/processFromUser.js
@@ -5,22 +5,48 @@ import { faFire } from '@fortawesome/free-solid-svg-icons';
 export default class ProcessFromUser extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            enterEditer: false,
+            dataBasse: [
+                { nameQuestion: ' مرادف كلمة ذكاء ؟ ', answers: [' مجرات ', ' انسان ', ' كوكب '], another: '', idQ: 'dWc243df' }
+            ]
+        }
     }
     render() {
         return (
+            <>
             <div className="NotesInfoPage">
                 <div className={"editeSections " + this.props.className}>
                     <div className="sectionsProcss">
                         <div className="sectionEditeFromUser">
-                            {this.props.learnWeb}
+                            <div onClick={() => this.setState({enterEditer : !this.state.enterEditer})}>{this.props.learnWeb}</div>
                         </div>
                     </div>
                     <div className="textTUsers">
-                        <FontAwesomeIcon icon={faFire}style={{ color: '#eb561b', fontSize: '15px' }} /> &nbsp;
+                        <FontAwesomeIcon icon={faFire} style={{ color: '#eb561b', fontSize: '15px' }} /> &nbsp;
                         {this.props.messToApper}
                     </div>
                 </div>
+            {this.state.enterEditer ? <IsQuAsk /> : ''}
             </div>
+            </>
         )
+        function IsQuAsk(s) {
+            function br(x) {
+                return {ssDataAn : "  answers  " , ssDataT : " title "}
+            }
+            return (
+                <div className="sectionQuAsk">
+                    <div className="_sectionQuAsk">
+                        <div className="titleQuestions">
+                            assadsas
+                        </div>
+                    </div>
+                    <div className="_sectionQuAsk">
+                        asdasd
+                    </div>
+                </div>
+            )
+        }
     }
 }

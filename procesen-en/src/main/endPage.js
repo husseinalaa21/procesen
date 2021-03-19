6a11dd2dc58cd719a84c64da034e8c9a023a55eb
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-03-20 01:30:42 +0200

    D

--- procesen-en/src/main/endPage.js
+++ procesen-en/src/main/endPage.js
@@ -6,8 +6,8 @@ export default function endPage(x) {
             <div className="about_info">
                 <div className="mainInfoAbout">
                     <div>
-                        <a><div className="infoDirPage"> call us </div></a>
-                        <a><div  className="infoDirPage"> Our support </div></a>
+                        <a><div className="infoDirPage"  href="https://callus.procesen.com/callus-en/index.html"> call us </div></a>
+                        <a><div  className="infoDirPage" href="https://paypal.me/husseinalaa2000?locale.x=ar_EG"> Our support </div></a>
                     </div>
                     <div>
                         <a href="#sec-definition-site"><div  className="infoDirPage"> About </div></a>
